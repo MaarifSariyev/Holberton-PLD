4fc47d190a411982419b9b0422bc9e4052cf0b23
MaarifSariyev <151127745+MaarifSariyev@users.noreply.github.com>
2024-10-03 15:04:43 +0400

    Create create-class.js

---
+++ Pld_classes/create-class.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(name, age, city){
+    this.name = name;
+    this.age = age;
+    this.city = city;
+  }//1ci sual
+
+  greet(){
+    return `Hello my name is ${name}, I am ${age} years old, and I live in ${city}`
+  }
+}//2ci sual
